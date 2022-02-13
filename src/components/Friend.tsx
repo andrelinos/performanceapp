c4264f9236547b253ee9da0aa90137fd5563a227
Andrelino Silva <andrelinocom@gmail.com>
2022-02-13 09:07:06 -0300

    Updating

--- src/components/Friend.tsx
+++ src/components/Friend.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { memo } from 'react';
 import { Text, StyleSheet } from 'react-native';
 
 interface ItemProps {
     data: {
+        id: number;
         name: string;
         likes: number;
     };
 }
 
-export function Friend({ data }: ItemProps) {
+function FriendComponent({ data }: ItemProps) {
     return (
         <Text style={styles.text}>
             {data.name} - Likes: {data.likes}
@@ -20,4 +21,9 @@ const styles = StyleSheet.create({
     text: {
         color: '#bdc3c7',
     },
+});
+
+// Recalcula apenas se o componente sofreu alterações
+export const Friend = memo(FriendComponent, (prevProps, nextProps) => {
+    return Object.is(prevProps.data, nextProps.data);
 });
