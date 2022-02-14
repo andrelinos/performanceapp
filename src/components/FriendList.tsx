a298ba637107f701eda7aa419be8818731785b13
Andrelino Silva <andrelinocom@gmail.com>
2022-02-14 08:05:32 -0300

    Add: memo, reduce e useMemo

--- src/components/FriendList.tsx
+++ src/components/FriendList.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useMemo } from 'react';
+import { View, Text } from 'react-native';
 
 import { Friend } from './Friend';
 
@@ -12,8 +12,17 @@ interface FriendProps {
 }
 
 export function FriendList({ data }: FriendProps) {
+    const totalLikes = useMemo(() => {
+        // likes = acumulador
+        // friend = de onde vai pegar os valores para memorizar
+        return data.reduce((likes, friend) => {
+            return likes + friend.likes;
+        }, 0);
+    }, [data]);
+
     return (
         <View>
+            <Text>Total de likes: {totalLikes}</Text>
             {data.map((friend) => (
                 <Friend key={String(friend.id)} data={friend} />
             ))}
